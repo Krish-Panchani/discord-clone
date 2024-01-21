0517ea2d22752cce465b7a1bd254e16287bf7bb5
Krish-Panchani <krishpanchani1346@gmail.com>
2024-01-21 22:59:14 +0530

    Added Auth using clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,13 +1,15 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 import { cn } from '@/lib/utils';
+import { UserButton } from '@clerk/nextjs';
 
 const state = false
 ;
 export default function Home() {
   return (
-    <div>
-      This is a protected Routes
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
+      protected route here
     </div>
   )
 }
