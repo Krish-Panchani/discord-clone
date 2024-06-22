b86faa5b6e497e16948e5fdee1feaedb3efe348e
Krish-Panchani <krishpanchani1346@gmail.com>
2024-06-22 20:00:11 +0530

    feat: Add FileUpload component for file upload functionality

---
+++ components/file-upload.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
+
+interface FileUploadProps {
+    // File upload component props
+    onChange: (url?: string) => void;
+    value?: string;
+    endpoint: "messageFile" | "serverImage";
+}
+
+export const FileUpload = ({
+    onChange,
+    value,
+    endpoint,
+}: FileUploadProps) => {
+    return (
+        <UploadDropzone
+
+            endpoint={endpoint}
+            onClientUploadComplete={(res) => {
+                onChange(res?.[0].url);
+            }}
+            onUploadError={(error: Error) => {
+                console.error("Upload error", error);
+            }}
+        />
+    );
+}
