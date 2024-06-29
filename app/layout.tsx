90a942878e6b7f5c9620b07db34898064064d167
Krish-Panchani <krishpanchani1346@gmail.com>
2024-06-29 20:10:08 +0530

    feat: Add ModelProvider component for providing models to the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/theme-provider" 
 import { cn } from "@/lib/utils";
+import { ModelProvider } from "@/components/providers/model-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModelProvider />
             {children}
           </ThemeProvider>
         </body>
