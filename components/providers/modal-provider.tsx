730f6ae64b8fd3501ad05c00387147a70468d2b9
Krish-Panchani <krishpanchani1346@gmail.com>
2024-12-08 20:19:41 +0530

    feat: Add InviteModel to ModalProvider for enhanced functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 
+import { InviteModel } from "@/components/modals/invite-modal";
 import { CreateServerModel } from "@/components/modals/create-server-modal";
 
 export const ModelProvider = () => {
@@ -18,6 +19,7 @@ export const ModelProvider = () => {
   return (
     <>
       <CreateServerModel />
+      <InviteModel />
     </>
   );
 };
