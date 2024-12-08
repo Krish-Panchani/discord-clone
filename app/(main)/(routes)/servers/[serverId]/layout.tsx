e89027f478674aa31553298fada42916750dbbd0
Krish-Panchani <krishpanchani1346@gmail.com>
2024-12-08 18:55:54 +0530

    feat: Integrate ServerSidebar component into ServerIdLayout for improved navigation

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -3,6 +3,7 @@ import { redirectToSignIn } from "@clerk/nextjs";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { ServerSidebar } from "@/components/server/ServerSidebar";
 
 const ServerIdLayout = async ({
   children,
@@ -36,13 +37,10 @@ const ServerIdLayout = async ({
 
   return (
     <div className="h-full">
-      <div 
-      className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-        Server Slidbar
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId} />
       </div>
-      <main className="h-full md:pl-60">
-        {children}
-        </main>
+      <main className="h-full md:pl-60">{children}</main>
     </div>
   );
 };
