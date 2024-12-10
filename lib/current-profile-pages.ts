ec718b823cbeda86aca17bb620bc1c2c39a6e5f1
Krish-Panchani <krishpanchani1346@gmail.com>
2024-12-10 16:41:39 +0530

    feat: Add currentProfile function to retrieve user profile from database

---
+++ lib/current-profile-pages.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest } from "next";
+import { getAuth } from "@clerk/nextjs/server";
+import { db } from "@/lib/db";
+
+
+export const currentProfile = async (req: NextApiRequest) => {
+
+    // Get the user ID from the session
+    const { userId } = getAuth(req);
+
+    // If the user is not logged in, return null
+    if (!userId) {
+        return null;
+    }
+
+    // Find the user's profile in the database
+    const profile = await db.profile.findUnique({
+        where: {
+            userId,
+        },
+    });
+
+    // If the profile is not found, return null otherwise return the profile
+    return profile;
+}
