e574d94416199e991027672974aaa7ab07a44ad4
Krish-Panchani <krishpanchani1346@gmail.com>
2024-12-11 22:52:16 +0530

    feat: Enhance PrismaClient configuration for improved logging and error formatting in development

--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,9 @@ declare global {
     var prisma: PrismaClient | undefined;
 };
 
-export const db = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+export const db = globalThis.prisma || new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ['query', 'info', 'warn', 'error'] : ['error'],
+    errorFormat: process.env.NODE_ENV === "development" ? 'pretty' : 'minimal',
+  });
+  
+  if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
